ddf2216a5634384e4f956366f45740cd2ec9d119
chengluyu <chengluyu@live.cn>
2017-11-27 01:12:28 +0800

    feat: introduce argparser

--- handwriting/recognize.py
+++ handwriting/recognize.py
@@ -1,13 +1,11 @@
+import argparse
 import cv2
 import numpy as np
 from svm import SVMPredictor
 from cnn import CNNPredictor
 
 def validate_size(width, height):
-    return 5 < width < 100 and 5 < height < 100
-
-# predictor = SVMPredictor('digits_cls.pkl')
-predictor = CNNPredictor('../mnist/model.h5')
+    return 5 < width < 100 or 5 < height < 100
 
 RECT_COLOR = (31, 16, 255)
 TEXT_COLOR = (9, 8, 9)
@@ -21,11 +19,13 @@ def deskew(img, size):
     img = cv2.warpAffine(img, M, (size, size), flags=cv2.WARP_INVERSE_MAP | cv2.INTER_LINEAR)
     return img
 
-
-def normalize(roi, weight, height):
-    margin_x = int(w * 0.4 if w > h else (h - w) / 2)
-    margin_y = int(h * 0.4 if h > w else (w - h) / 2)
-    roi = cv2.copyMakeBorder(roi, margin_y, margin_y, margin_x, margin_x, cv2.BORDER_CONSTANT, 255)
+def normalize(roi, width, height):
+    side_length = int(1.4 * max(width, height))
+    top = int(side_length - height) // 2
+    bottom = side_length - height - top
+    left = int(side_length - width) // 2
+    right = side_length - width - left
+    roi = cv2.copyMakeBorder(roi, top, bottom, left, right, cv2.BORDER_CONSTANT, 255)
     roi = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
     roi = cv2.dilate(roi, (3, 3))
     roi = deskew(roi, 28)
@@ -37,24 +37,46 @@ def show_single_prediction(roi, result):
     cv2.imshow('debug', roi)
     cv2.waitKey()
 
-if __name__ == '__main__':
-    origin = cv2.imread('IMG_2988.jpg')
-    origin = cv2.resize(origin, (origin.shape[1] // 2, origin.shape[0] // 2))
+def parse_argv():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('image')
+    parser.add_argument('--method', dest='method')
+    parser.add_argument('--show-digits', dest='show_digits', action='store_true')
+    return parser.parse_args()
+
+def select_predictor(method):
+    if method == 'cnn':
+        return CNNPredictor('../mnist/model.h5')
+    elif method == 'hog':
+        return SVMPredictor('./digits_cls.pkl')
+    raise RuntimeError('unsupported method')
+
+def preprocess(origin):
     image = cv2.fastNlMeansDenoisingColored(origin, None, 10, 10, 7, 21)
     image = cv2.cvtColor(origin, cv2.COLOR_BGR2GRAY)
     image = cv2.GaussianBlur(image, (3, 3), 0)
-    _, image = cv2.threshold(image, 90, 255, cv2.THRESH_BINARY_INV)
-    cv2.imshow('Binarized Image', image)
-    _, contours, hier = cv2.findContours(image.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    for (x, y, w, h) in map(cv2.boundingRect, contours):
+    ret, image = cv2.threshold(image, 90, 255, cv2.THRESH_BINARY_INV)
+    # image = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+    return image
+
+def segmentation(image):
+    ret, contours, hierarchy = cv2.findContours(image.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    return map(cv2.boundingRect, contours)
+
+if __name__ == '__main__':
+    args = parse_argv()
+    predictor = select_predictor(args.method)
+    raw = cv2.imread(args.image)
+    cooked = preprocess(raw)
+    for (x, y, w, h) in segmentation(cooked):
         if not validate_size(w, h):
             continue
-        cv2.rectangle(origin, (x, y), (x + w, y + h), RECT_COLOR, 1)
-        roi = normalize(image[y:y+h,x:x+w], w, h)
+        cv2.rectangle(raw, (x, y), (x + w, y + h), RECT_COLOR, 1)
+        roi = normalize(cooked[y:y+h,x:x+w], w, h)
         result = predictor.predict(roi)
-        show_single_prediction(roi, result)
-        cv2.putText(origin, str(int(result)), (x, y), cv2.FONT_HERSHEY_PLAIN, 2, TEXT_COLOR, 2)
-
+        if args.show_digits:
+            show_single_prediction(roi, result)
+        cv2.putText(raw, str(int(result)), (x + w // 2, y), cv2.FONT_HERSHEY_PLAIN, 2, TEXT_COLOR, 2)
     cv2.namedWindow('Resulting Image with Rectangular ROIs', cv2.WINDOW_NORMAL)
-    cv2.imshow('Resulting Image with Rectangular ROIs', origin)
+    cv2.imshow('Resulting Image with Rectangular ROIs', raw)
     cv2.waitKey()
